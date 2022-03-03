4bb5314fd045b80de9b676303b044233b507198b
Antonov Kirill <antonovkir2@gmail.com>
2022-03-03 10:29:39 +0500

    unit useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import Button from "./components/Button";
 
 
 function App() {
 
-    const Button1Foo = (sub: string, age: number) => {
-        console.log(sub, age)
+    // let a = 1;
+    let [a, setA] = useState(1);
+
+    const incr =()=> {
+        setA(++a);
+        console.log(a)
     }
-    const Button2Foo = (sub: string, city: string) => {
-        console.log(sub, city)
+
+    const decr =()=>{
+        setA(--a);
     }
-    const Button3Foo = () => {
-        console.log('BTN')
+
+    const noll =()=> {
+        setA(a=0);
     }
 
     return (
         <div>
-            {/*<button>My YouTubeChanel-1</button>*/}
-            {/*<button>My YouTubeChanel-2</button>*/}
-            <Button name={'My YouTubeChanel-1'} callBack={() => Button1Foo('Kirill', 28)}/>
-            <Button name={'My YouTubeChanel-2'} callBack={() => Button2Foo('Ivan', 'Sugrut')}/>
-            <Button name={'My YouTubeChanel-3'} callBack={Button3Foo}/>
+            <h1>{a}</h1>
+            <button onClick={incr}>+</button>
+            <button onClick={decr}>-</button>
+            <button onClick={noll}>0</button>
         </div>
     );
 }
