3ce841ab98ef74d32cd50fcedd664b3064f9836d
Antonov Kirill <antonovkir2@gmail.com>
2022-03-12 14:05:09 +0500

    useState react kabzda 20m

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,15 @@
 import React, {useState} from 'react';
 import './App.css';
-import {Input} from "./components/Input";
-import Button from "./components/Button";
+import OnOff from "./components/OnOff";
 
 
 function App() {
-
-    let [message, setMessage] = useState([
-        {message: 'message1'},
-        {message: 'message2'},
-        {message: 'message3'},
-    ])
-
-    let [title, setTitle] = useState('')
-
-    const addMessage = (title:string) => {
-        let newMessage = {message: title};
-        setMessage([newMessage, ...message])
-    }
-
-    const callbackButtonHandler = () => {
-        addMessage(title)
-        setTitle('')
-    }
-
     return (
-        <div className={'App'}>
-            <Input setTitle={setTitle} title={title}/>
-            <Button name={'+'} callback={callbackButtonHandler}/>
-
-            {message.map((m, i) => {
-                return (
-                    <div key={i}>{m.message}</div>
-                )
-            })}
+        <div>
+            <OnOff on={false}/>
+            <OnOff on={true}/>
         </div>
-    );
+    )
 }
 
 export default App;
---
+++ src/components/OnOff.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+type OnOffPropsType = {
+    on:boolean
+}
+
+const OnOff: React.FC<OnOffPropsType> = (props) => {
+
+    const onStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid #000',
+        display: 'inline-block',
+        padding: '2px',
+        backgroundColor: props.on ? 'green' : 'white'
+    }
+    const offStyle = {
+        width: '30px',
+        height: '20px',
+        border: '1px solid #000',
+        display: 'inline-block',
+        marginLeft: '2px',
+        padding: '2px',
+        backgroundColor: props.on ? 'white' : 'red'
+    }
+    const indicatorStyle = {
+        width: '10px',
+        height: '10px',
+        borderRadius: '50%',
+        border: '1px solid #000',
+        display: 'inline-block',
+        marginLeft: '5px',
+        backgroundColor: props.on ? 'green' : 'red'
+    }
+
+    return (
+        <div>
+            <div style={onStyle}>On</div>
+            <div style={offStyle}>Off</div>
+            <div style={indicatorStyle}></div>
+        </div>
+    );
+};
+
+export default OnOff;
