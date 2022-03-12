6fec2c60213c6f67c627ca834c8d13de90a8c128
Antonov Kirill <antonovkir2@gmail.com>
2022-03-12 15:43:20 +0500

    remove kabzda tasks in new rep

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.css';
-import OnOff from "./components/OnOff";
-import {Rating} from "./components/Rating";
+
 
 
 function App() {
     return (
         <div>
-            <OnOff/>
-            <Rating/>
+
         </div>
     )
 }
